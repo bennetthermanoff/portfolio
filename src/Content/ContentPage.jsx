01e5ab7eea2812a0f366c644565896ae87e0f06e
Bennett Hermanoff <bennetthermanoff@gmail.com>
2022-12-07 19:43:21 -0600

    upload test img

--- src/Content/ContentPage.jsx
+++ src/Content/ContentPage.jsx
@@ -1,15 +1,54 @@
 import React from 'react'
-import {useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import './ContentPage.css'
+import t1 from './markdown/1.md'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { useEffect, useState } from 'react';
+
 
 const ContentPage = () => {
-    let { id } = useParams();
+    const [contents, setContents] = useState(
+        [
+        {
+            title: 'test1',
+            id: '1',
+            content: '',
+        },
+        {
+            title: 'test2',
+            id: '2',
+            content: '',
+        },
+    ]
+    );
 
+
+
+    let { id } = useParams();
+    //on page load
+    useEffect(() => {
+        contents.forEach(x => {
+            fetch(t1)
+                .then(response => response.text())
+                .then(text => {
+                    console.log(text);
+                    setContents(contents.map(y => {
+                        if (y.id === x.id) {
+                            y.content = text;
+                        }
+                        return y;
+                    }))
+                })
+        })
+    }, []) 
     return (
         <div className='App-header'>
 
             <div className='contentBackgroundColumn'>
-                hello
+                
+                <ReactMarkdown remarkPlugins={[remarkGfm]} children={String(contents.find(x => x.id === id).content)} />
+                
             </div>
             <h className='headerBarText'>
                 Bennett Hermanoff
@@ -21,5 +60,7 @@ const ContentPage = () => {
     )
 
 }
+
+
 
 export { ContentPage }
