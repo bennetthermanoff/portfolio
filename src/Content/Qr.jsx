bb676f3b8f2da73b1604c514f4c2a030467bc2e4
Bennett Hermanoff <bennetthermanoff@gmail.com>
2023-01-29 19:24:18 -0600

    qr

---
+++ src/Content/Qr.jsx
@@ -0,0 +1,28 @@
+import {QRCodeCanvas, QRCodeSVG} from 'qrcode.react';
+import './ContentPage.css'
+import {useState} from 'react';
+const QrPage = ()=>{
+    const [qrValue, setQrValue] = useState('https://hermanoff.dev/');
+
+    const onChange = (e)=>{
+        const value = e.target.value;
+        //if value does not start with http:// or https://, add https:// to the beginning
+        // if(value.startsWith('http://') || value.startsWith('https://')){
+        //     setQrValue(value);
+        // }else{
+        //     setQrValue('https://'+value);
+        // }
+        setQrValue(value);
+    }
+
+    return(
+        <div className='content'>
+            <div className='contentBackgroundColumn'>
+            <QRCodeSVG value={qrValue} className='QR' size={500}/>
+            <input type='text' className='QRInput' placeholder='Enter URL here' onChange={onChange} value={qrValue}/>
+            </div>
+        </div>
+        
+    )
+}
+export {QrPage};
